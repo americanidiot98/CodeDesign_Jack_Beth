fa9027f926331edd6841330f524e1ab2d1dbcaba
Bethbush98 <36197432+americanidiot98@users.noreply.github.com>
2018-02-20 13:33:04 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'header.inc.php';?>
+<body>
+  
+  
+   <header>
+       <h1>Beth's Page</h1>
+   </header>
+   
+   <?php include 'nav.inc.php';?>
+   
+   
+   <?php include 'hgroup.inc.php';?>
+   
+   
+   <footer>
+       <?php include 'footer.inc.php';?>
+   </footer>  
+</body>
+</html>
